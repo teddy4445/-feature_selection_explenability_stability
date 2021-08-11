6b1f0e575b9f6c0c09c80660a05e9c16609b0b2a
hilit <hilitaharoni@gmail.co>
2021-08-11 18:56:57 +0300

    data in one hot encoding

--- data_sets_prepare.py
+++ data_sets_prepare.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from glob import glob
 
+from pandas.core.dtypes.common import is_numeric_dtype
+
 
 class DataSetPrepare:
     """
@@ -41,20 +43,32 @@ class DataSetPrepare:
             df = pd.read_csv(file_path)
             # move target column to the right (last column)
             target_col = df.pop('target')
-            df['target'] = target_col
+
             # fix columns
             col_to_remove = []
             for col in list(df):
                 unique_count = df[col].nunique()
-                if df[col].dtype != np.number:
+                if not is_numeric_dtype(df[col]):
                     if unique_count / df[col].size > DataSetPrepare.MAX_RATIO or unique_count > DataSetPrepare.MAX_UNIQUE_VALUES:
                         col_to_remove.append(col)
                     else:
+                        new_cols_names = list(df[col].unique())
+                        for new_name in new_cols_names:
+                            df[new_name] = 0
+                            df.loc[df[col] == new_name, new_name] = 1
+                        col_to_remove.append(col)
                         # TODO: maybe use one hot encoding instead
-                        mapper = {value: index for index, value in enumerate(list(df[col].unique()))}
-                        df[col] = df[col].apply(lambda x: mapper[x])
+                        # mapper = {value: index for index, value in enumerate(list(df[col].unique()))}
+                        # df[col] = df[col].apply(lambda x: mapper[x])
             # remove what we do not need
             df.drop(col_to_remove, axis=1, inplace=True)
+
+            # add targets to the right
+            df['target'] = target_col
+            # convert target to integers
+            mapper = {value: index for index, value in enumerate(list(df['target'].unique()))}
+            df['target'] = df['target'].apply(lambda x: mapper[x])
+
             # Finally, when we have only the data we need, remove lines with nan
             df.dropna(inplace=True)
             # save the result
@@ -63,4 +77,4 @@ class DataSetPrepare:
 
 if __name__ == '__main__':
     DataSetPrepare.run(data_sets_source_folder_path=os.path.join(os.path.dirname(__file__), "data"),
-                       data_sets_target_folder_path=os.path.join(os.path.dirname(__file__), "data_fixed"))
+                       data_sets_target_folder_path=os.path.join(os.path.dirname(__file__), "data_fixed_one_hot"))
