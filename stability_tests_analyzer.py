6891c5844070d38057448a91324fa8ac82054263
Teddy Lazebnik <lazebnik.teddy@gmail.com>
2021-08-20 02:23:48 +0300

    stability analyzer with more plotS

--- stability_tests_analyzer.py
+++ stability_tests_analyzer.py
@@ -67,15 +67,21 @@ class StabilityTestsAnalyzer:
 
         3. For each row in the MDF:
            error bar plot: x-axis the data stability test (with names as ticks)
-                     y-axis the mean test score of the FS sets distance metric across the FS algorithms
-                            and with an error bar corresponding to one STD.
-
-        4. For all rows in the MDF:
-           error bar plot: x-axis the FS algorithm (with names as ticks) AND bar-groups according to the data stability test
-                           y-axis the mean test score the FS sets distance metric across the rows in the MDF
-                                  and with an error bar corresponding to one STD.
+                           y-axis the mean test score of the FS sets distance metric across the FS algorithms
+                           and with an error bar corresponding to one STD.
            ** The z-values needs to be normalized between 0 and 1
         """
+        # prepare inner folders
+        threed_prints_folder_name = "3d_plots"
+        instance_bar_prints_folder_name = "instance_bar_plots"
+        stability_instance_bar_prints_folder_name = "stability_instance_bar_plots"
+        inner_folders = [threed_prints_folder_name, instance_bar_prints_folder_name, stability_instance_bar_prints_folder_name]
+        for folder_name in inner_folders:
+            try:
+                os.mkdir(os.path.join(results_folder_path, folder_name))
+            except:
+                pass
+
         # first, find all the FS algorithms + stability tests
         fs_algorithms = {}
         stability_tests = {}
@@ -102,7 +108,8 @@ class StabilityTestsAnalyzer:
             z_delta = max(z) - z_min
             y_size = len(y)
             x_size = len(x)
-            z = np.asarray([round((z_value - z_min) / z_delta, 3) for z_value in z]).reshape(y_size, x_size)
+            z = [round((z_value - z_min) / z_delta, 3) for z_value in z]
+            three_d_z = np.asarray(z).reshape(y_size, x_size)
             # make 2d grid
             x, y = np.meshgrid(x, y)
             # plot the surface.
@@ -110,7 +117,7 @@ class StabilityTestsAnalyzer:
                                    figsize=(10, 10))
             ax.plot_wireframe(x,
                               y,
-                              z,
+                              three_d_z,
                               rstride=1,
                               cstride=1,
                               color="black",
@@ -118,8 +125,8 @@ class StabilityTestsAnalyzer:
                               cmap=cm.coolwarm)
             ax.scatter(x,
                        y,
-                       z,
-                       c=z,
+                       three_d_z,
+                       c=three_d_z,
                        s=80,
                        cmap=cm.coolwarm)
             # format the z-axis in a cooler way
@@ -131,7 +138,61 @@ class StabilityTestsAnalyzer:
             ax.set_xticklabels([value.replace("_", " ") for value in list(fs_algorithms.keys())])
             ax.view_init(28, 30)
             # save the plot
-            plt.savefig(os.path.join(results_folder_path, "{}.png".format(row["ds_name"])))
+            plt.savefig(os.path.join(results_folder_path, threed_prints_folder_name, "{}.png".format(row["ds_name"])))
+            # close for next plot
+            plt.close()
+
+            # save data as a bar plot #
+            x = np.arange(len(fs_algorithms))  # the label locations
+            width = 0.9/len(stability_tests)  # the width of the bars
+            # prepare plot
+            fig, ax = plt.subplots()
+            for stability_test_index, stability_test in enumerate(stability_tests):
+                rects = ax.bar(x - 0.45 + width * stability_test_index,
+                               z[stability_test_index::len(stability_tests)],
+                               width,
+                               label=stability_test)
+                # TODO: add the following line to have numbers on top
+                # ax.bar_label(rects, padding=3)
+
+            # Add some text for labels, title and custom x-axis tick labels, etc.
+            ax.set_ylabel("{} [1]".format(stability_metric))
+            ax.set_xticks(x)
+            ax.set_yticks(np.linspace(0, 1, 11))
+            ax.set_xticklabels([value.replace("_", " ") for value in list(fs_algorithms.keys())])
+            ax.legend()
+            plt.grid(axis="y",
+                     alpha=0.5)
+            plt.xticks(rotation=90)
+            fig.tight_layout()
+            # save the plot
+            plt.savefig(os.path.join(results_folder_path, instance_bar_prints_folder_name, "{}_bar_plot.png".format(row["ds_name"])))
+            # close for next plot
+            plt.close()
+
+            # save data as a bar plot with stability test as the leading #
+            x = np.arange(len(stability_tests))  # the label locations
+            width = 0.9
+            # prepare plot
+            fig, ax = plt.subplots()
+            rects = ax.bar(x,
+                           [np.nanmean([z[stability_test_index::len(stability_tests)]]) for stability_test_index in range(len(stability_tests))],
+                           yerr=[np.nanstd([z[stability_test_index::len(stability_tests)]]) for stability_test_index in range(len(stability_tests))],
+                           width=width,
+                           capsize=5)
+            # TODO: add the following line to have numbers on top
+            # ax.bar_label(rects, padding=3)
+
+            # Add some text for labels, title and custom x-axis tick labels, etc.
+            ax.set_ylabel("{} [1]".format(stability_metric))
+            ax.set_xticks(x)
+            ax.set_xticklabels(list(stability_tests.keys()))
+            plt.grid(axis="y",
+                     alpha=0.5)
+            plt.xticks(rotation=0)
+            fig.tight_layout()
+            # save the plot
+            plt.savefig(os.path.join(results_folder_path, stability_instance_bar_prints_folder_name, "{}_stability_bar_plot.png".format(row["ds_name"])))
             # close for next plot
             plt.close()
 
@@ -146,7 +207,38 @@ class StabilityTestsAnalyzer:
         are the 'x' features of each row in the MDF and a 'y' feature which is one stability-related column.
         * This process repeats for every 'y' feature which is one stability-related column in the MDF.
         """
-        pass
+        # prepare method's inner folder
+        inner_folder_name = "hierarchical_clustering_plots"
+        try:
+            os.mkdir(os.path.join(results_folder_path, inner_folder_name))
+        except:
+            pass
+        # load the fs algorithms and stability tests
+        fs_algorithms = {}
+        stability_tests = {}
+        column_names = list(mdf)
+        stability_column_names = []
+        x_column_names = []
+        for column_name in column_names:
+            # get the x represented columns
+            if "x-" in column_name:
+                x_column_names.append(column_name)
+            try:
+                stability_metric, fs_filter, stability_test = StabilityTestsAnalyzer._get_column_signature(
+                    column_name=column_name)
+                if fs_filter not in fs_algorithms:
+                    fs_algorithms[fs_filter] = len(fs_algorithms.keys())
+                if stability_test not in stability_tests:
+                    stability_tests[stability_test] = len(stability_tests.keys())
+                stability_column_names.append(column_name)
+            except:
+                pass
+
+        # format for each row the representing x-vector of it
+        ds_embeddings = []
+        for index, row in mdf.iterrows():
+            # TODO : make imbedding and produce the graphs
+            # ds_embeddings.append()
 
     @staticmethod
     def prepare_dataset_for_classificator_learning(mdf,
