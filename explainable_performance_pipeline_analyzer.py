7267487c5a84a4fcc94b6e87ae60bdf9b9c66ee8
hilit <hilitaharoni@gmail.co>
2021-08-24 00:16:46 +0300

    added lazypredict for classification

--- explainable_performance_pipeline_analyzer.py
+++ explainable_performance_pipeline_analyzer.py
@@ -1,6 +1,7 @@
 # library imports
 import os
 import pandas as pd
+import numpy as np
 
 # project imports
 from meta_data_table_generator import MetaDataTableGenerator
@@ -41,7 +42,7 @@ class ExplainablePerformancePipelineAnalyzer:
                              sub_tables_folder_path: str):
         """
         This method drops all irrelevant columns in the MDF and divide the
-        'x' columns and the 'y' columns that starts with 'expandability_' in the dataframe.
+        'x' columns and the 'y' columns that starts with 'expandability-' in the dataframe.
 
         Afterward, it divide the dataframe to the 'x' columns and the 'y' columns with
         the results of a singular metric at a time. Each such dataframe is saved with name that
@@ -59,9 +60,9 @@ class ExplainablePerformancePipelineAnalyzer:
 
         # create sub-table for every metric from MetaDataTableGenerator.METRICS
         for metric in MetaDataTableGenerator.METRICS:
-            # create sub_mdf using the columns with x_ and with the relevant metric (__ replaced by _)
+            # create sub_mdf using the columns with x_ and with the relevant metric
             sub_mdf = mdf[[c for c in mdf.columns if
-                           c.startswith("x-") or (c.startswith("expandability_") and metric.replace("__", "_") in c)]]
+                           c.startswith("x-") or (c.startswith("expandability-") and metric in c)]]
             sub_mdf.to_csv(os.path.join(sub_tables_folder_path, "sub_table_for_{}.csv".format(metric)))
 
     @staticmethod
@@ -73,7 +74,7 @@ class ExplainablePerformancePipelineAnalyzer:
         """
         # read sub_mdf
         for filename in os.listdir(sub_tables_folder_path):
-            sub_mdf = pd.read_csv(os.path.join(sub_tables_folder_path, filename))
+            sub_mdf = pd.read_csv(os.path.join(sub_tables_folder_path, filename)).drop('Unnamed: 0', axis=1)
 
             # find column with maximal value
             sub_mdf['best'] = sub_mdf[[c for c in sub_mdf.columns if not c.startswith('x-')]].idxmax(axis="columns")
@@ -103,8 +104,16 @@ class ExplainablePerformancePipelineAnalyzer:
             sub_mdf = pd.read_csv(os.path.join(sub_tables_folder_path, filename))
             X = sub_mdf[[c for c in sub_mdf.columns if c.startswith("x-")]]
             y = sub_mdf['best']
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.8, random_state=123)
-            clf = LazyClassifier(verbose=0, ignore_warnings=True, custom_metric=None)
+
+            # clean the data and fix anomalies
+            X.replace(np.inf, 1e9, inplace=True)
+            X.replace(-np.inf, -1e9, inplace=True)
+            y.replace(np.inf, 1e9, inplace=True)
+            y.replace(-np.inf, -1e9, inplace=True)
+
+
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=123)
+            clf = LazyClassifier(verbose=0, ignore_warnings=True, custom_metric=None, predictions=True)
             scores, predictions = clf.fit(X_train, X_test, y_train, y_test)
 
             scores.to_csv(os.path.join(results_folder_path, "scores_for_" + filename))
