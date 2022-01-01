63f7415570c9302d12e2a30f0dcce2eb730fc2b1
Teddy Lazebnik <lazebnik.teddy@gmail.com>
2022-01-01 13:16:38 +0200

    paper plot fix

--- paper/paper_plots.py
+++ paper/paper_plots.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,57 +8,98 @@ def plot_no_free_lunch():
     df = pd.read_csv(r"C:\Users\lazeb\Desktop\-feature_selection_explenability_stability\meta_table_data\meta_dataset.csv")
     relevant_df = df[[col for col in list(df) if "expandability" not in col and "acc" in col]]
     pipeline_names = list(relevant_df)
-    pipeline_index_best = []
-    fs_filter_index_best = []
-    fs_embedded_index_best = []
+
+    pipeline_index_best = {item.replace("accuracy-", "").replace("-", ",").replace("_", " "): 0 for item in pipeline_names}
+    fs_names = ["chi_square",
+                 "symmetrical_uncertainty",
+                 "information_gain",
+                 "pearson_correlation",
+                 "spearman_correlation",
+                 "remove_low_variance",
+                 "missing_value_ratio",
+                 "fishers_score"]
+    fs_filter_index_best = {item.replace("_", " "): 0 for item in fs_names}
+    embbeded_names = ["decision_tree",
+                    "lasso",
+                    "linear_svc"]
+    fs_embedded_index_best = {item.replace("_", " "): 0 for item in embbeded_names}
     for row_index, row in relevant_df.iterrows():
-        best_index = np.nanargmax(row)
-        pipeline_index_best.append(best_index)
-        fs_filter_index_best.append(best_index % 8)
-        fs_embedded_index_best.append(best_index % 3)
+        new_row = [val + random.random()/1000 for val in row]
+        best_index = np.nanargmax(new_row)
+        pipeline_index_best[pipeline_names[best_index].replace("accuracy-", "").replace("-", ",").replace("_", " ")] += 1
+        fs_filter_index_best[fs_names[best_index % 8].replace("_", " ")] += 1
+        fs_embedded_index_best[embbeded_names[best_index % 3].replace("_", " ")] += 1
 
     # hist count
-    values, counts = np.unique(pipeline_index_best, return_counts=True)
-    plt.bar(range(len(values)),
-            [val/len(pipeline_index_best) for val in counts],
+    keys = list(pipeline_index_best.keys())
+    values = [pipeline_index_best[key] for key in keys]
+    count = sum(values)
+    plt.bar(range(len(pipeline_index_best)),
+            [val/count for val in values],
             width=0.8,
             color="black")
     plt.xlabel("FS Pipeline")
-    plt.xlabel("Probability to be the optimal pipeline")
+    plt.ylabel("Probability to be the optimal pipeline")
     plt.grid(color="black",
              axis="y",
              alpha=0.1)
-    # plt.xticks(range(len(pipeline_names)), pipeline_names)
+    keys = [key.replace("chi square", "CS")
+                .replace("symmetrical uncertainty", "SU")
+                .replace("pearson correlation", "PC")
+                .replace("remove low variance", "RLV")
+                .replace("missing value ratio", "MVR")
+                .replace("spearman correlation", "SC")
+                .replace("fishers score", "FS")
+                .replace("information gain", "IG") for key in keys]
+    keys = [key.replace("decision tree", "DT").replace("lasso", "L").replace("linear svc", "SVC") for key in keys]
+    plt.xticks(range(len(keys)), keys, rotation=90)
+    plt.tight_layout()
     plt.savefig("full_pipeline.png")
     plt.close()
 
     # hist count
-    values, counts = np.unique(fs_filter_index_best, return_counts=True)
-    plt.bar(range(len(values)),
-            [val/len(fs_filter_index_best) for val in counts],
+    keys = list(fs_filter_index_best.keys())
+    values = [fs_filter_index_best[key] for key in keys]
+    count = sum(values)
+    plt.bar(range(len(fs_filter_index_best)),
+            [val/count for val in values],
             width=0.8,
             color="black")
     plt.xlabel("Filter FS algorithm")
-    plt.xlabel("Probability to be the optimal Filter FS algorithm")
+    plt.ylabel("Probability to be the optimal Filter FS algorithm")
     plt.grid(color="black",
              axis="y",
              alpha=0.1)
-    # plt.xticks(range(len(pipeline_names)), pipeline_names)
+
+    keys = [key.replace("chi square", "CS")
+                .replace("symmetrical uncertainty", "SU")
+                .replace("pearson correlation", "PC")
+                .replace("remove low variance", "RLV")
+                .replace("missing value ratio", "MVR")
+                .replace("spearman correlation", "SC")
+                .replace("fishers score", "FS")
+                .replace("information gain", "IG") for key in keys]
+    plt.xticks(range(len(keys)), keys, rotation=90)
+    plt.tight_layout()
     plt.savefig("filter_fs.png")
     plt.close()
 
     # hist count
-    values, counts = np.unique(fs_embedded_index_best, return_counts=True)
+    keys = list(fs_embedded_index_best.keys())
+    values = [fs_embedded_index_best[key] for key in keys]
+    count = sum(values)
     plt.bar(range(len(values)),
-            [val/len(fs_embedded_index_best) for val in counts],
+            [val/count for val in values],
             width=0.8,
             color="black")
     plt.xlabel("Embedded FS algorithm")
-    plt.xlabel("Probability to be the optimal Embedded FS algorithm")
+    plt.ylabel("Probability to be the optimal Embedded FS algorithm")
     plt.grid(color="black",
              axis="y",
              alpha=0.1)
-    # plt.xticks(range(len(pipeline_names)), pipeline_names)
+    keys = [key.replace("decision tree", "DT").replace("lasso", "L").replace("linear svc", "SVC") for key in keys]
+    plt.xticks(range(len(keys)), keys, rotation=90)
+    plt.tight_layout()
     plt.savefig("embedded_fs.png")
     plt.close()
 
